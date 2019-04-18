45e32fcf301c4bb0c32ee3760764b7ae5235474e
Pranav <p_thirunavukkarasu@u.pacific.edu>
2019-04-17 18:21:00 -0700

    Added Update Profile and Get Profile routes

--- src/routes/profiles.js
+++ src/routes/profiles.js
@@ -22,6 +22,8 @@ router.get('/', (req, res) => {
 router.post('/create', (req, res) => {
   let resObj = resForm();
 
+  let user_id = req.body.user_id;
+
   let profile = {
     user_id: req.body.user_id,
     firstname: req.body.firstname,
@@ -31,12 +33,27 @@ router.post('/create', (req, res) => {
     class: req.body.class,
     major: req.body.major,
     location: req.body.location,
+    bio: req.body.bio,
     tags: req.body.tags,
     image: req.body.image
   };
 
   // TODO check for required fields
-  profilesService.createProfile(profile).then((mongRes) => {
+  
+  // check for existing 
+  profilesService.getProfileByUser(user_id).then((mongRes) => {
+    const err = mongRes.error;
+
+    console.log("ERR");
+    console.log(JSON.stringify(mongRes));
+
+    if (err === null) {
+      // Profile for user exists
+      return profilesService.updateProfile(profile);
+    }
+
+    return profilesService.createProfile(profile);
+  }).then((mongRes) => {
     const err = mongRes.error;
 
     if (err) {
@@ -50,6 +67,38 @@ router.post('/create', (req, res) => {
     }
   }).catch((err) => {
 
+    // Fail
+    resObj.success = false;
+    resObj.data = null;
+    resObj.error = err;
+    return res.status(200).json(resObj);
+  });
+});
+
+
+/*
+ * Get Profile by user_id
+ * Method: GET
+  * */
+router.get('/getByUserId', (req, res) => {
+  let resObj = resForm();
+
+  let user_id = req.query.user_id;
+  console.log('ID ' + user_id);
+
+  profilesService.getProfileByUser(user_id).then((mongRes) => {
+    const err = mongRes.error;
+
+    if (err) {
+      throw 'Cannot retrieve Profile';
+    } else {
+      // Success
+      resObj.success = true;
+      resObj.data = mongRes.data;
+      resObj.error = null;
+      return res.status(200).json(resObj);
+    }
+  }).catch((err) => {
     // Fail
     resObj.success = false;
     resObj.data = null;
--- src/service/profiles.js
+++ src/service/profiles.js
@@ -19,10 +19,9 @@ const MongoSrc = require('./MongoSrc.js');
 async function createProfile(profile) {
   let resObj = resForm();
 
-
   // query command
   let query = {
-    user_id: profile.user_id,
+    user_id: new MongoObjectID(profile.user_id),
     firstname: profile.firstname,
     lastname: profile.lastname,
     age: profile.age,
@@ -57,11 +56,101 @@ async function createProfile(profile) {
   }
 }
 
+// update profile
+async function updateProfile(profile) {
+  let resObj = resForm();
+
+  // filter command
+  let filter = {
+    user_id: new MongoObjectID(profile.user_id)
+  };
+
+  // query command
+  let query = {
+    user_id: new MongoObjectID(profile.user_id),
+    firstname: profile.firstname,
+    lastname: profile.lastname,
+    age: profile.age,
+    gender: profile.gender,
+    class: profile.class,
+    major: profile.major,
+    location: profile.location,
+    bio: profile.bio,
+    tags: profile.tags,
+    image: profile.image
+  };
+
+  try {
+    const client = await MongoClient.connect(MongoSrc);
+    const db = client.db('RoomMatch');
+    const res = await db.collection('profiles').updateOne(filter, { $set : query });
+    await client.close();
+
+    // Success
+    console.log('Success: updated profile to database')
+    resObj.success = true;
+    resObj.data = res;
+    resObj.error = null;
+    return resObj;
+  } catch(err) {
+    // Fail
+    console.log("ERROR: Cannot updated Profile");
+    console.log(err);
+    resObj.success = false;
+    resObj.data = null;
+    resObj.error = err;
+    return resObj;
+  }
+}
+
 // Get single profile by user_id
 async function getProfileByUser(user_id) {
+  let resObj = resForm();
+
+  // query command
+  let query = {
+    user_id: new MongoObjectID(user_id)
+  };
+
+  try {
+    const client = await MongoClient.connect(MongoSrc);
+    const db = client.db('RoomMatch');
+    const res = await db.collection('profiles').findOne(query);
+    await client.close();
+
+    if (res) {
+      console.log('Success: retrieved profile from database')
+
+      // profile found
+      resObj.success = true;
+      resObj.data = res;
+      resObj.error = null;
+      return resObj;
+    } else {
+      // profile not found
+      resObj.success = false;
+      resObj.data = null;
+      resObj.error = "No profile found";
+      return resObj;
+    }
+  } catch(err) {
+    // Fail
+    console.log("ERROR: Cannot retrieve Profile");
+    console.log(err);
+    resObj.success = false;
+    resObj.data = null;
+    resObj.error = err;
+    return resObj;
+  }
+}
+
+// upload profile image to DB
+async function createProfileImage(image) {
   
 }
 
 module.exports = {
-  createProfile: createProfile
+  createProfile: createProfile,
+  updateProfile: updateProfile,
+  getProfileByUser: getProfileByUser
 };
